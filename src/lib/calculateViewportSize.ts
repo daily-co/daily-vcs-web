cffb8be35f9175fc3c86d6e06c97c269e1acce3e
harshithpabbati <pabbatiharshith@gmail.com>
2023-07-17 18:10:26 +0530

    use ternary operator

--- src/lib/calculateViewportSize.ts
+++ src/lib/calculateViewportSize.ts
@@ -2,16 +2,12 @@ export const calculateViewportSize = (
   element: HTMLElement,
   aspectRatio: number
 ): { w: number; h: number } => {
-  let size = { w: 0, h: 0 };
-
-  if (!element || !aspectRatio || aspectRatio === 0) return size;
+  if (!element || !aspectRatio || aspectRatio === 0) return { w: 0, h: 0 };
 
   const { width, height } = element.getBoundingClientRect();
   const originalAspectRatio = width / height;
 
-  if (originalAspectRatio > aspectRatio) {
-    size = { w: Math.floor(height * aspectRatio), h: height };
-  } else size = { w: width, h: Math.floor(width / aspectRatio) };
-
-  return size;
+  return originalAspectRatio > aspectRatio
+    ? { w: Math.floor(height * aspectRatio), h: height }
+    : { w: width, h: Math.floor(width / aspectRatio) };
 };
