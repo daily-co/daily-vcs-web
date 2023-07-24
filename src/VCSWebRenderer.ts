2cd1711bd8836ff237736544a3b32675f044ca73
Harshith Pabbati <pabbatiharshith@gmail.com>
2023-07-24 16:31:01 +0530

    bind handleParticipantsChange to preserve the context

--- src/VCSWebRenderer.ts
+++ src/VCSWebRenderer.ts
@@ -309,15 +309,33 @@ export default class DailyVCSWebRenderer {
   }
 
   private setupEventListeners() {
-    this.callObject.on('participant-joined', this.handleParticipantsChange);
-    this.callObject.on('participant-updated', this.handleParticipantsChange);
-    this.callObject.on('participant-left', this.handleParticipantsChange);
+    this.callObject.on(
+      'participant-joined',
+      this.handleParticipantsChange.bind(this)
+    );
+    this.callObject.on(
+      'participant-updated',
+      this.handleParticipantsChange.bind(this)
+    );
+    this.callObject.on(
+      'participant-left',
+      this.handleParticipantsChange.bind(this)
+    );
   }
 
   private removeEventListeners() {
-    this.callObject.off('participant-joined', this.handleParticipantsChange);
-    this.callObject.off('participant-updated', this.handleParticipantsChange);
-    this.callObject.off('participant-left', this.handleParticipantsChange);
+    this.callObject.off(
+      'participant-joined',
+      this.handleParticipantsChange.bind(this)
+    );
+    this.callObject.off(
+      'participant-updated',
+      this.handleParticipantsChange.bind(this)
+    );
+    this.callObject.off(
+      'participant-left',
+      this.handleParticipantsChange.bind(this)
+    );
   }
 
   /**
